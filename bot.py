3ef10a40cd72501f08f2e0899c990f0855fa1384
Mikhail Muchko <muchkom@gmail.com>
2021-04-04 15:41:30 +0300

    init commit

---
+++ bot.py
@@ -0,0 +1,137 @@
+from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup, Update, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+import logging
+import solver
+import uuid
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+ANSWER = 0
+
+
+def start(update, context: CallbackContext):
+    update.message.reply_text('Бот для решения паззла BallSortPuzzle ')
+
+
+def echo(update: Update, context: CallbackContext):
+    """Echo the user message."""
+    print(update.message)
+
+
+reply_keyboard = [
+    ['/forward', '/backward', '/begin', '/end'],
+]
+
+
+def to_solve(update: Update, context: CallbackContext):
+    filename = uuid.uuid4().__str__()
+    file = context.bot.getFile(update.message.photo[-1].file_id)
+    file.download(filename)
+    try:
+        way = solver.analise(filename)
+    except Exception as err:
+        update.message.reply_text(
+            'Exception was found. Exception msg: {}'.format(err.args),
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard),
+        )
+        return
+
+    os.remove(filename)
+
+    context.user_data['way'] = way
+    context.user_data['index'] = 0
+
+    update.message.reply_text(
+        'Answer was found. To navigate use text command or keyboard',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard),
+    )
+    print_current(update, way, 0)
+
+    return ANSWER
+
+
+def cancel(update: Update, _):
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return ConversationHandler.END
+
+
+def flask_to_str(source):
+    return f'{source % 7 + 1}{"↑" if source // 7 == 1 else "↓"}'
+
+
+def print_current(update, way, index):
+    s, t, c = way[index]
+    update.message.reply_text(
+        text='From {} to {}. Step {} from {}'.format(
+            flask_to_str(s),
+            flask_to_str(t),
+            index + 1,
+            len(way)
+        ),
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard),
+    )
+
+
+def answer(update: Update, context):
+    way = context.user_data['way']
+    index = context.user_data['index']
+
+    if update.message.text == '/forward':
+        if index < len(way):
+            index += 1
+    elif update.message.text == '/backward':
+        if index > 0:
+            index -= 1
+    elif update.message.text == '/begin':
+        index = 0
+    elif update.message.text == '/end':
+        update.message.reply_text(
+            'Bye Bye',
+            reply_markup=ReplyKeyboardRemove(),
+        )
+        return ConversationHandler.END
+
+    context.user_data['index'] = index
+    print_current(update, way, index)
+    return ANSWER
+
+
+def main():
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    updater = Updater(os.environ.get('token'))
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("cancel", cancel))
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[MessageHandler(Filters.photo, to_solve)],
+            states={
+                ANSWER: [
+                    MessageHandler(Filters.regex('^/(forward|backward|begin|end)$'), answer),
+                    MessageHandler(Filters.photo, to_solve),
+                ]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)],
+        )
+    )
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
