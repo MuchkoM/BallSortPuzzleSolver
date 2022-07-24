9f07ca70908f650470ee60cfaa26c565e6375e06
Petr Giloian <petr.giloian@vwfspay.com>
2022-07-25 01:03:28 +0400

    refactoring -- move complexity of printing into FiledPrinter class

--- solver/field_printer.py
+++ solver/field_printer.py
@@ -1,15 +1,113 @@
+from __future__ import annotations
+
 import copy
+import io
 import sys
+from typing import List, Tuple
 
 from solver.field import Field
 from solver.palette import Palette
 from solver.utils import colored_text, get_alpha
 
 
+# def printer(array, row_num=None, next_line=7, header1="", header2=""):
+#     if row_num is None:
+#         row_num = len(array[0])
+#
+#     lines = lines_normalizer(array, row_num)
+#
+#     for line in lines:
+#         print(*line, sep=' | ')
+#
+#
+# def lines_normalizer(array, row_num):
+#     work_array = copy.deepcopy(array)
+#
+#     for val in work_array:
+#         cur_num = len(val)
+#         if cur_num != row_num:
+#             for i in range(row_num - cur_num):
+#                 val.append(' ')
+#
+#     lines = []
+#     for i in range(row_num):
+#         lines.append([])
+#
+#     for i, col in enumerate(work_array):
+#         for j, el in enumerate(col):
+#             lines[row_num - j - 1].append(el)
+#
+#     return lines
+#
+#
+# def flask_to_str(source, divider):
+#     div, mod = divmod(source, divider)
+#     return f'{mod + 1}{"↓" if div == 1 else "↑"}'
+#
+#
+# def print_split(lines, row_num, divider, header=False, stream=sys.stdout):
+#     splatted = [[], []]
+#     for group in splatted:
+#         for i in range(row_num):
+#             group.append([])
+#
+#     for i, line in enumerate(lines):
+#         for j, el in enumerate(line):
+#             if j < divider:
+#                 splatted[0][i].append(el)
+#             else:
+#                 splatted[1][i].append(el)
+#
+#     for cur_lines in splatted:
+#         for i, line in enumerate(cur_lines):
+#             print(*transform_line(line), sep='   ' if header & (i == 0) else ' | ', file=stream)
+#
+#
+# def print_solution(start, way, stream=sys.stdout):
+#     curr_start = copy.deepcopy(start)
+#     col_num = len(curr_start)
+#     row_num = len(curr_start[0])
+#     divider = col_num // 2
+#
+#     for i, step in enumerate(way):
+#         s, t, c = step
+#         print('From {} to {}. Step {} from {}'.format(
+#             flask_to_str(s, divider),
+#             flask_to_str(t, divider),
+#             i + 1,
+#             len(way)
+#         ), file=stream)
+#
+#         header = []
+#         for j in range(col_num):
+#             cur_symbol = ' '
+#             if j == s:
+#                 cur_symbol = '↑'
+#             if j == t:
+#                 cur_symbol = '↓'
+#             header.append(cur_symbol)
+#
+#         lines = lines_normalizer(curr_start, row_num)
+#         lines.insert(0, header)
+#
+#         print_split(lines, row_num + 1, divider, header=True)
+#
+#         push(curr_start, s, t)
+#     print("\nSolved:", file=stream)
+#     lines = lines_normalizer(curr_start, row_num)
+#     print_split(lines, row_num, divider)
+
+
 class FieldPrinter:
     def __init__(self, field: Field, palette: Palette = None):
         self.field = field
         self.palette = palette
+        self.field_filler = ' | '
+        self.header_filler = '   '
+        self.header_empty = ' '
+
+        self.src_header_filler = '↑'
+        self.des_header_filler = '↓'
 
     def lines_normalizer(self, array, row_num):
         work_array = copy.deepcopy(array)
@@ -30,11 +128,22 @@ class FieldPrinter:
 
         return lines
 
-    def print(self, header: str = None, footer: str = None, stream=sys.stdout):
+    def prepare_header(self, src, des, el) -> List[str]:
+        header_arr = [self.header_empty] * self.field.column
+        header_arr[src] = colored_text(self.palette.get_color_by_index(el), self.src_header_filler)
+        header_arr[des] = colored_text(self.palette.get_color_by_index(el), self.des_header_filler)
+
+        return header_arr
+
+    def print(self, header: Tuple[str] | None = None, footer: str | None = None, stream=sys.stdout):
+        str_io = io.StringIO()
         lines = self.lines_normalizer(self.field.field, self.field.dimension)
 
         if header:
-            print(header, file=stream)
+            if isinstance(header, tuple):
+                str_io.write(self.header_filler.join(self.prepare_header(*header)) + '\n')
+            else:
+                str_io.write(header + '\n')
 
         for line in lines:
             symbols = []
@@ -46,9 +155,10 @@ class FieldPrinter:
                         symbols.append(symbol)
                 else:
                     symbols.append(symbol)
-            print(*symbols, sep=' | ', file=stream)
+            str_io.write(self.field_filler.join(symbols) + '\n')
         if footer:
-            print(footer, file=stream)
+            str_io.write(footer + '\n')
+        print(str_io.getvalue(), file=stream, end='')
 
 
 if __name__ == '__main__':
--- solver/solution_printer.py
+++ solver/solution_printer.py
@@ -1,98 +1,11 @@
+from __future__ import annotations
+
 import io
 
 from solver.field import Field
 from solver.field_printer import FieldPrinter
 from solver.palette import Palette
-from solver.utils import colored_text
 from solver.way import Way
-
-
-# def printer(array, row_num=None, next_line=7, header1="", header2=""):
-#     if row_num is None:
-#         row_num = len(array[0])
-#
-#     lines = lines_normalizer(array, row_num)
-#
-#     for line in lines:
-#         print(*line, sep=' | ')
-#
-#
-# def lines_normalizer(array, row_num):
-#     work_array = copy.deepcopy(array)
-#
-#     for val in work_array:
-#         cur_num = len(val)
-#         if cur_num != row_num:
-#             for i in range(row_num - cur_num):
-#                 val.append(' ')
-#
-#     lines = []
-#     for i in range(row_num):
-#         lines.append([])
-#
-#     for i, col in enumerate(work_array):
-#         for j, el in enumerate(col):
-#             lines[row_num - j - 1].append(el)
-#
-#     return lines
-#
-#
-# def flask_to_str(source, divider):
-#     div, mod = divmod(source, divider)
-#     return f'{mod + 1}{"↓" if div == 1 else "↑"}'
-#
-#
-# def print_split(lines, row_num, divider, header=False, stream=sys.stdout):
-#     splatted = [[], []]
-#     for group in splatted:
-#         for i in range(row_num):
-#             group.append([])
-#
-#     for i, line in enumerate(lines):
-#         for j, el in enumerate(line):
-#             if j < divider:
-#                 splatted[0][i].append(el)
-#             else:
-#                 splatted[1][i].append(el)
-#
-#     for cur_lines in splatted:
-#         for i, line in enumerate(cur_lines):
-#             print(*transform_line(line), sep='   ' if header & (i == 0) else ' | ', file=stream)
-#
-#
-# def print_solution(start, way, stream=sys.stdout):
-#     curr_start = copy.deepcopy(start)
-#     col_num = len(curr_start)
-#     row_num = len(curr_start[0])
-#     divider = col_num // 2
-#
-#     for i, step in enumerate(way):
-#         s, t, c = step
-#         print('From {} to {}. Step {} from {}'.format(
-#             flask_to_str(s, divider),
-#             flask_to_str(t, divider),
-#             i + 1,
-#             len(way)
-#         ), file=stream)
-#
-#         header = []
-#         for j in range(col_num):
-#             cur_symbol = ' '
-#             if j == s:
-#                 cur_symbol = '↑'
-#             if j == t:
-#                 cur_symbol = '↓'
-#             header.append(cur_symbol)
-#
-#         lines = lines_normalizer(curr_start, row_num)
-#         lines.insert(0, header)
-#
-#         print_split(lines, row_num + 1, divider, header=True)
-#
-#         push(curr_start, s, t)
-#     print("\nSolved:", file=stream)
-#     lines = lines_normalizer(curr_start, row_num)
-#     print_split(lines, row_num, divider)
 
 
 class SolutionPrinter:
@@ -111,7 +24,7 @@ class SolutionPrinter:
         for i, (src, des, el) in enumerate(self.way):
             self.add_step(
                 field=current,
-                header=self.header(src, des, el),
+                header=(src, des, el),
                 footer='Step {} out {}'.format(i + 1, len(self.way))
             )
 
@@ -122,19 +35,12 @@ class SolutionPrinter:
             footer='Solved in {} steps'.format(len(self.way))
         )
 
-    def add_step(self, field, header, footer):
+    def add_step(self, field, header=None, footer=None):
         printer = FieldPrinter(field, self.palette)
         str_io = io.StringIO()
         printer.print(header, footer, str_io)
 
         self.step_list.append(str_io.getvalue())
-
-    def header(self, src, des, el):
-        header_arr = [' '] * self.field.column
-        header_arr[src] = colored_text(self.palette.get_color_by_index(el), '↑')
-        header_arr[des] = colored_text(self.palette.get_color_by_index(el), '↓')
-
-        return '   '.join(header_arr)
 
     def __len__(self):
         return len(self.step_list)
@@ -144,7 +50,7 @@ class SolutionPrinter:
 
     def print_way(self):
         for step in self.step_list:
-            print(step)
+            print(step, end='')
 
 
 if __name__ == '__main__':
